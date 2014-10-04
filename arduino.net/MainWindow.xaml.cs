634b091ab65269a8a678af6b75b23b7748539a35
dave < dsuarezv@gmail.com>
2014-10-04 12:17:30 +0200

    Added new textbox library. It's winforms, but performance is a lot better.

--- arduino.net/MainWindow.xaml.cs
+++ arduino.net/MainWindow.xaml.cs
@@ -264,49 +264,9 @@ namespace arduino.net
             });
 
             InitDwarf();
-
-            UpdateWatches();
         }
 
-        private void UpdateWatches()
-        {
-            Dispatcher.Invoke(() =>
-            {
-                var t = MemDumpPad1.ResultTextBlock;
-                t.Text = "";
-                t.Text += GetWatchValue("myGlobalVariable");
-                t.Text += GetWatchValue("mylocal");
-                t.Text += GetWatchValue("result");
-            });
-        }
-
-        private string GetWatchValue(string symbolName)
-        {
-            var pc = IdeManager.Debugger.Registers.Registers["PC"];
-            var function = IdeManager.Dwarf.GetFunctionAt(pc);
-            if (function == null) return symbolName + ": <context not found>\n";
-
-            return GetWatchValue(function, symbolName);
-        }
-
-        private string GetWatchValue(string functionName, string symbolName)
-        {
-            var function = IdeManager.Dwarf.GetFunctionByName(functionName);
-            if (function == null) return symbolName + ": <context not found>\n";
-
-            return GetWatchValue(function, symbolName);
-        }
-
-        private string GetWatchValue(DwarfSubprogram function, string symbolName)
-        {
-            var symbol = IdeManager.Dwarf.GetSymbol(symbolName, function);
-            if (symbol == null) return symbolName + ": <not found>\n";
-
-            var val = symbol.GetValue(IdeManager.Debugger);
-            if (val == null) return symbolName + ": <symbol has no location>\n";
-
-            return string.Format("{0}: {1}\n", symbolName, symbol.GetValueRepresentation(IdeManager.Debugger, val));
-        }
+        
 
 
         private void Debugger_TargetConnected(object sender)
